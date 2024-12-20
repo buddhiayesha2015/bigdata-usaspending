8907e462eb9e562d11b02ff3c75a5fcd1ac13f4d
Buddhi Ayesha <buddhiayesha2013@gmail.com>
2024-12-20 18:35:35 -0500

    Add logging format

--- app/logging_config.py
+++ app/logging_config.py
@@ -1,24 +1,23 @@
 import logging
-import logging.handlers
 
 
-# Configure the logging
 def setup_logging():
-    logger = logging.getLogger()
+    logger = logging.getLogger("usaspending")
+    if logger.hasHandlers():
+        return logger
+
     logger.setLevel(logging.INFO)
 
-    # Create a file handler
+    # Create file handler
     file_handler = logging.FileHandler("bdt_usaspending_app.log")
     file_handler.setLevel(logging.INFO)
 
-    # Create a console handler
+    # Create console handler
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
 
-    # Define a log format
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+    # Create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
